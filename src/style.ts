2fc445a16f1aeb89f55a08ba3df10d0066c7ff9e
Yota Toyama <raviqqe@gmail.com>
2018-07-23 09:42:22 -0700

    Change box-sizing

--- src/style.ts
+++ src/style.ts
@@ -2,6 +2,10 @@ import { injectGlobal } from "styled-components";
 
 // tslint:disable no-unused-expression
 injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     font: 16px sans-serif;
